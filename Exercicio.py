58c3eae66a13bd5965a539e79371e6ffde7b279d
Pedrocoro0601 <pedro.17461@alunofpm.com.br>
2022-11-29 15:04:45 -0300

    criado exercicio
    Adicionado entrada e saída de dados.

---
+++ Exercicio.py
@@ -0,0 +1,12 @@
+nome = input("Digite seu nome: ")
+idade = input("digite sua idade: ")
+altura = float(input("digite sua altura: "))
+peso = float(input("digite seu peso: "))
+
+
+print("-" * 30)
+print("Este são meus dados: ")
+print("nome: " ,nome)
+print("idade: " ,idade, "anos")
+print("altura" ,altura, "mts")
+print("peso",peso, "kgs")
---
+++ Operadores matemáticos.py
@@ -0,0 +1,9 @@
+n1= float(input("digite o primeiro numero"))
+n2= float(input("digite o segundo numero"))
+
+print("soma = ", n1 + n2)
+print("soma = ", n1 - n2)
+print("soma = ", n1 * n2)
+print("soma = ", n1 / n2)
+print("qudrado do 1", n1**2)
+print("quadrado do 2", n1**2) 
